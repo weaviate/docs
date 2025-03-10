d307f457e0e20d9e155dab864c73526ce7eaf170
Sebastian Witalec <sebawita@users.noreply.github.com>
2025-03-10 18:38:42 +0100

    add Netlify workflow to the new docs

---
+++ _build_scripts/publish-prod-to-netlify.sh
@@ -0,0 +1,11 @@
+# #!/bin/bash
+set -e
+
+echo "TODO: UNCOMMENT BELOW to allow publishing this repo to production."
+echo "NOTE: you may need to figure out GOOGLE KEYS for the sitemap"
+
+# # deploy
+# ./node_modules/.bin/netlify deploy --prod --dir=build --site=weaviate-io
+
+# # Share sitemap to G
+# curl https://www.google.com/ping?sitemap=https://weaviate.io/sitemap.xml
---
+++ _build_scripts/slack-release-message.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Get commit message
+git_hash=$(echo "$GITHUB_SHA" | cut -c1-7)
+commit_message="$(git log -1 $git_hash --pretty="%s")"
+
+# Replace &, <, and > – as per Slack API instructions
+commit_message=${commit_message//&/&amp;}
+commit_message=${commit_message//</&lt;}
+commit_message=${commit_message//>/&gt;}
+
+# Prepare the message and send it to Slack
+MESSAGE="{ 'text': 'Hey $AUTHOR_NAME - your :docusaurus: :weaviate-logo: website update is live at: 🔥 https://weaviate.io 🔥 \n> $commit_message' }"
+
+echo $MESSAGE > payload_release.json
+
+echo "TODO: Comment out the below code to send a SLACK message for main updates"
+
+# Send the slack message
+# curl -X POST -H 'Content-type: application/json' -d @payload_release.json https://hooks.slack.com/services/$SLACK_BOT
