06fdce646c1d0e83b276c26b4f8f06d91cf2044d
Sebastian Witalec <sebawita@users.noreply.github.com>
2025-03-11 11:52:22 +0100

    add new netlify site

--- _build_scripts/publish-prod-to-netlify.sh
+++ _build_scripts/publish-prod-to-netlify.sh
@@ -1,11 +1,9 @@
 # #!/bin/bash
 set -e
 
-echo "TODO: UNCOMMENT BELOW to allow publishing this repo to production."
-echo "NOTE: you may need to figure out GOOGLE KEYS for the sitemap"
-
 # # deploy
-# ./node_modules/.bin/netlify deploy --prod --dir=build --site=weaviate-io
+./node_modules/.bin/netlify deploy --prod --dir=build --site=weaviate-docs
 
+echo "TODO: you may need to figure out GOOGLE KEYS for the sitemap"
 # # Share sitemap to G
 # curl https://www.google.com/ping?sitemap=https://weaviate.io/sitemap.xml
