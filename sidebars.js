5aa5784784a50da17c88e2aa20a1afba8d7da6f1
g-despot <66276597+g-despot@users.noreply.github.com>
2025-05-23 11:37:55 +0200

    Update docs

--- sidebars.js
+++ sidebars.js
@@ -736,14 +736,34 @@ const sidebars = {
     {
       type: "category",
       label: "Installation Guides",
-      collapsible: true,
-      collapsed: true,
+      className: "sidebar-main-category",
+      collapsible: false,
       items: [
-        "deploy/installation-guides/k8s-installation",
-        "deploy/installation-guides/docker-installation",
-        "deploy/installation-guides/gcp-installation",
-        "deploy/installation-guides/aws-installation",
-        "deploy/installation-guides/aws-cli",
+        {
+          type: "doc",
+          id: "deploy/installation-guides/k8s-installation",
+          className: "sidebar-item",
+        },
+        {
+          type: "doc",
+          id: "deploy/installation-guides/docker-installation",
+          className: "sidebar-item",
+        },
+        {
+          type: "doc",
+          id: "deploy/installation-guides/gcp-installation",
+          className: "sidebar-item",
+        },
+        {
+          type: "doc",
+          id: "deploy/installation-guides/aws-installation",
+          className: "sidebar-item",
+        },
+        {
+          type: "doc",
+          id: "deploy/installation-guides/aws-cli",
+          className: "sidebar-item",
+        },
       ],
     },
   ],
