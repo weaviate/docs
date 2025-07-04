ad29672baf7623cdbbfb5255a93728f7729a4187
g-despot <66276597+g-despot@users.noreply.github.com>
2025-07-04 10:47:21 +0200

    Update robots.txt setup

--- _build_scripts/publish-prod-to-netlify.sh
+++ _build_scripts/publish-prod-to-netlify.sh
@@ -1,22 +1,12 @@
-# #!/bin/bash
+#!/bin/bash
 set -e
 
-#### TODO: REMOVE 👇 BEFORE WE GO LIVE ####
-# For Netlify - change robot.txt to Disallow - so that crawlers will ignore it
-mv build/robots.txt build/robots.txt.live
-echo -e "User-agent: *
-Disallow: /" >> build/robots.txt
-#### TODO: REMOVE 👆 BEFORE WE GO LIVE ####
-
-# # deploy
-./node_modules/.bin/netlify deploy --prod --dir=build --site=docs-weaviate-io
-
-echo "TODO: you may need to figure out GOOGLE KEYS for the sitemap"
-# # Share sitemap to G
-# curl https://www.google.com/ping?sitemap=https://weaviate.io/sitemap.xml
-
-#### TODO: REMOVE 👇 BEFORE WE GO LIVE ####
-# Bring back the original robots.txt file
+# Make sure crawlers are allowed
 rm build/robots.txt
 mv build/robots.txt.live build/robots.txt
-#### TODO: REMOVE 👆 BEFORE WE GO LIVE ####
+
+# Deploy to Netlify
+./node_modules/.bin/netlify deploy --prod --dir=build --site=docs-weaviate-io
+
+# Share sitemap to Google
+curl https://www.google.com/ping?sitemap=https://docs.weaviate.io/sitemap.xml
