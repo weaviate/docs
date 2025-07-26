01223d497999d2afd9b843883f17811a7d0e09dc
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2025-07-26 15:47:57 +0200

    Fix failing

--- _includes/code/python/local.quickstart.create_collection.py
+++ _includes/code/python/local.quickstart.create_collection.py
@@ -13,19 +13,17 @@ client.collections.delete("Question")
 # highlight-start
 questions = client.collections.create(
     name="Question",
-    vector_config=Configure.Vectors.text2vec_ollama(     # Configure the Ollama embedding integration
-        api_endpoint="http://ollama:11434",       # If using Docker you might need: http://host.docker.internal:11434
-        model="nomic-embed-text",                               # The model to use
+    vector_config=Configure.Vectors.text2vec_ollama(  # Configure the Ollama embedding integration
+        api_endpoint="http://ollama:11434",  # If using Docker you might need: http://host.docker.internal:11434
+        model="nomic-embed-text",  # The model to use
     ),
-    generative_config=Configure.Generative.ollama(              # Configure the Ollama generative integration
-        api_endpoint="http://ollama:11434",       # If using Docker you might need: http://host.docker.internal:11434
-        model="llama3.2",                                       # The model to use
-    )
+    generative_config=Configure.Generative.ollama(  # Configure the Ollama generative integration
+        api_endpoint="http://ollama:11434",  # If using Docker you might need: http://host.docker.internal:11434
+        model="llama3.2",  # The model to use
+    ),
 )
 # highlight-end
 # CreateCollection
 
 client.close()  # Free up resources
 # END CreateCollection
-
-
