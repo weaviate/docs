780af595021a3913606e2b02c12f36c7117afb42
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2025-07-27 18:48:14 +0200

    Test query agent

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -8,8 +8,8 @@ from pathlib import Path
 @pytest.mark.parametrize(
     "script_loc",
     [
-        #"./docs/agents/_includes/query_agent.py",
-        "./docs/agents/_includes/transformation_agent.py"
+        "./docs/agents/_includes/query_agent.py",
+        #"./docs/agents/_includes/transformation_agent.py"
     ],
 )
 def test_on_blank_instance_pyv4(empty_weaviates, script_loc):
