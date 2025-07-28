9f01d57cee978c6668a7429942a6f1b33b7445fc
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2025-07-28 12:30:40 +0200

    Remove broken code

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -9,7 +9,7 @@ from pathlib import Path
     "script_loc",
     [
         "./docs/agents/_includes/query_agent.py",
-        #"./docs/agents/_includes/transformation_agent.py"
+        "./docs/agents/_includes/transformation_agent.py"
     ],
 )
 def test_on_blank_instance_pyv4(empty_weaviates, script_loc):
