266bfafb16140244524be163b16a24057b7664a7
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2025-07-29 12:23:10 +0200

    Update docs

--- _includes/code/csharp/ConnectionTests.cs
+++ _includes/code/csharp/ConnectionTests.cs
@@ -11,8 +11,35 @@ public class ConnectionTest
     public async Task Should_Connect_To_Weaviate()
     {
         // START LocalNoAuth
-        var client = Connect.Local(restPort: 8085, grpcPort: 50055);
+        var client = Connect.Local(restPort: 8080, grpcPort: 50051);
         // END LocalNoAuth
+        // try
+        // {
+        //     var meta = await client.GetMeta();
+        //     Assert.NotNull(meta);
+        //     Assert.NotNull(client);
+        // }
+        // catch (Exception ex)
+        // {
+        //     Assert.True(false, $"Connection failed: {ex.Message}");
+        // }
+    }
+
+    [Fact]
+    public async Task Should_Custom_Connect_To_Weaviate()
+    {
+        // START CustomConnect
+         var config = new ClientConfiguration(
+            RestAddress: "localhost",
+            GrpcAddress: "localhost", 
+            RestPort: 8080,
+            GrpcPort: 50051,
+            UseSsl: false,
+            ApiKey: null
+        );
+        
+        var client = new WeaviateClient(config);
+        // END CustomConnect
         // try
         // {
         //     var meta = await client.GetMeta();
