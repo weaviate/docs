ddd43fd6a568c94bac713b52b4d38b09fb67f110
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-08-06 14:30:40 +0100

    Script Update

---
+++ src/scripts/hubspot.js
@@ -0,0 +1,13 @@
+// src/scripts/hubspot.js
+const hubspotScript = {
+  tagName: "script",
+  attributes: {
+    type: "text/javascript",
+    id: "hs-script-loader",
+    async: true,
+    defer: true,
+    src: "//js.hs-scripts.com/8738733.js",
+  },
+};
+
+module.exports = hubspotScript;
