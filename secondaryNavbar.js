1db7bea0fa69a82455076a90c3268bbf0a5ed3d3
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2025-10-31 11:59:40 +0100

    Update docs

--- secondaryNavbar.js
+++ secondaryNavbar.js
@@ -67,7 +67,6 @@ const secondaryNavbarItems = {
       { label: "Configuration", link: "/deploy/configuration", sidebar: "deployConfigSidebar"},
       /*{ label: "Kubernetes", link: "/deploy/k8s", sidebar: "deployK8sSidebar" },*/
       { label: "Production Guides", link: "/deploy/production", sidebar: "deployProductionSidebar"},
-      { label: "Tutorials", link: "/deploy/tutorials", sidebar: "deployTutorialSidebar"},
       { label: "AWS", link: "/deploy/aws", sidebar: "deployAwsSidebar" },
       /*{ label: "Scaling Strategies", link: "/deploy/scaling-strategies", sidebar: "deployScalingSidebar"},*/
       /*{ label: "Monitoring and Observability", link: "/deploy/monitoring-obs", sidebar: "deployObservabilitySidebar"},*/
