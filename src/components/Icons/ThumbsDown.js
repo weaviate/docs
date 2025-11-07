7ccf19b989f832115972112be0c68e88495e4cac
JP Hwang <me@jphwang.com>
2025-11-07 13:24:34 +0000

    icons

--- src/components/Icons/ThumbsDown.js
+++ src/components/Icons/ThumbsDown.js
@@ -12,8 +12,8 @@ export default function ThumbsDown() {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M7 14V2" />
-      <path d="M15 18.12 14 14H8.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 10.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z" />
+      <path d="M17 14V2" />
+      <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22a3.13 3.13 0 0 1-3-3.88Z" />
     </svg>
   );
 }
