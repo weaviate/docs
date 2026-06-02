8915b9f681f21c57e121e46b817f51dad0e56c6d
Ivan Despot <66276597+g-despot@users.noreply.github.com>
2026-06-02 08:17:32 +0200

    Add REST query docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
         cdn: "https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.49.0",
         configuration: {
           spec: {
-            url: "https://raw.githubusercontent.com/weaviate/weaviate/openapi-for-docs/openapi-specs/schema.json",
+            url: "https://raw.githubusercontent.com/weaviate/weaviate/rest-query-split-endpoints-docs/openapi-specs/schema.json",
           },
           hideModels: true,
           // showSidebar: true,
